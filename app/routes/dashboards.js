28818d98f53b53cf31c2ee31d667697f835a5292
Steve Montana <stephen.a.montana@gmail.com>
2016-03-15 03:45:19 -0500

    Add new Daily Lease Report

--- app/routes/dashboards.js
+++ app/routes/dashboards.js
@@ -1,13 +1,61 @@
 var express = require('express');
+var moment = require('moment');
 require('locus');
+
 
 var Hashids = require("hashids");
 var hashids = new Hashids("LeAh Is GoInG pLaCeS rtbbly", 12);
 
-module.exports = function(connection, models) {
+module.exports = function (connection, models) {
   var router = express.Router();
 
-  router.get('/:uid', function(req, res, next){
+  router.get('/new', function(req, res, next){
+    var title = 'New Daily Leasing Report';
+
+    res.render('../views/dashboards/new', {
+      title: title
+    });
+  });
+
+  router.post('/', function (req, res, next){
+    var form_params = req.body;
+    var report_date = form_params.report_date;
+    var report_date_formatted = moment(report_date).format("YYYY-MM-DD");
+
+    // console.log(JSON.stringify(req.body, null, 3));
+
+    var submission = {
+      report_date: report_date_formatted,
+      prelease_percentage_prv_year: form_params.prelease_percentage_prv_year,
+      traffic_count_today: form_params.traffic_today,
+      traffic_count_week: form_params.traffic_week,
+      guarantor_received_count: form_params.guarantor_received_count,
+      new_lease_count_today: form_params.new_lease_count_today,
+      new_lease_count_week: form_params.new_lease_count_week,
+      new_lease_count_YTD: form_params.new_lease_count_YTD,
+      renewal_count_today: form_params.renewal_count_today,
+      renewal_count_week: form_params.renewal_count_week,
+      renewal_count_YTD: form_params.renewal_count_YTD,
+      leased_floorplan_1x1: form_params.leased_floorplan_1x1,
+      leased_floorplan_2x2a: form_params.leased_floorplan_2x2a,
+      leased_floorplan_2x2b: form_params.leased_floorplan_2x2b,
+      leased_floorplan_4x2: form_params.leased_floorplan_4x2,
+      NOT_leased_floorplan_1x1: form_params.NOT_leased_floorplan_1x1,
+      NOT_leased_floorplan_2x2a: form_params.NOT_leased_floorplan_2x2a,
+      NOT_leased_floorplan_2x2b: form_params.NOT_leased_floorplan_2x2b,
+      NOT_leased_floorplan_4x2: form_params.NOT_leased_floorplan_4x2
+    };
+
+    var new_record = models.DailyReport.build(submission);
+
+    new_record.save().then(function(new_record) {
+
+      var redirect_path = '/dashboards/' + new_record.uid;
+      res.redirect(redirect_path);
+    });
+  });
+
+  router.get('/:uid', function (req, res, next){
     var uid = req.params.uid;
     var id = hashids.decode(uid);
 
@@ -25,7 +73,6 @@ module.exports = function(connection, models) {
       var renewal_percentage = parseFloat(report.renewal_percentage());
       var prelease_percentage = parseFloat(report.prelease_percentage());
       var prelease_percentage_prv_year = parseFloat(report.prelease_percentage_prv_year);
-      // eval(require('locus'));
 
       guarantor_percentage = (guarantor_percentage * 100).toFixed(2);
       new_lease_percentage = (new_lease_percentage * 100).toFixed(2);
@@ -63,7 +110,7 @@ module.exports = function(connection, models) {
   var page;
   var per_page;
 
-  router.get('/', function(req, res, next){
+  router.get('/', function (req, res, next){
     page = parseInt(req.query.page) || 1;
     per_page = parseInt(req.query.per_page) || 20;
     var page_offset = (page - 1) * per_page;
