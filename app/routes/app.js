05b2a467de88437095ef1023babfaae97bc5f109
Steve Montana <stephen.a.montana@gmail.com>
2016-06-12 01:06:24 -0500

    added homepage

--- app/routes/app.js
+++ app/routes/app.js
@@ -3,10 +3,14 @@ var express = require('express');
 module.exports = function(connection, models) {
   var router = express.Router();
 
-  
-
   router.get('/', function(req, res, next){
-    res.redirect('/dashboards');
+    var title = 'Home'
+    // res.redirect('/dashboards');
+    res.render('../views/app/index', {
+      title: title
+    });
+
+
   });
 
   return(router);
